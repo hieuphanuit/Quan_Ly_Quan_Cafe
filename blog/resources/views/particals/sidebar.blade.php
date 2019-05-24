e78cc95834337d28e7b7fa45cebb378d9af896cb
Phan Đình Minh Hiếu <16520412@gm.uit.edu.vn>
2019-05-24 16:50:12 +0700

    update side bar

--- blog/resources/views/particals/sidebar.blade.php
+++ blog/resources/views/particals/sidebar.blade.php
@@ -1,60 +1,59 @@
 <!-- Sidebar -->
 <ul class="sidebar navbar-nav">
-  <li class="nav-item active">
+  <li class="nav-item  {{Request::is('dashboard') ? 'active' : null}}">
     <a class="nav-link" href="/dashboard">
       <i class="fas fa-fw fa-tachometer-alt"></i>
       <span>Dashboard</span>
     </a>
   </li>
-  <li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" href="/nhanvien" id="pagesDropdown" >
+  <li class="nav-item {{Request::is('nhanvien') ? 'active' : null}}">
+    <a class="nav-link" href="/nhanvien">
       <i class="fas fa-fw fa-folder"></i>
       <span>Nhân Viên</span>
     </a>
   </li>
-    <li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" href="/khachhangthanthiet" id="pagesDropdown">
+  <li class="nav-item {{Request::is('khachhangthanthiet') ? 'active' : null}}">
+    <a class="nav-link" href="/khachhangthanthiet">
       <i class="fas fa-fw fa-folder"></i>
       <span>Khách Hàng</span>
     </a>
   </li>
-  <li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" href="/thucdon" id="pagesDropdown">
+  <li class="nav-item {{Request::is('thucdon') ? 'active' : null}}">
+    <a class="nav-link" href="/thucdon">
       <i class="fas fa-fw fa-folder"></i>
       <span>Thực Đơn</span>
     </a>
   </li>
-  <li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" href="/kho" id="pagesDropdown">
+  <li class="nav-item {{Request::is('kho') ? 'active' : null}}">
+    <a class="nav-link" href="/kho">
       <i class="fas fa-fw fa-folder"></i>
       <span>Kho</span>
     </a>
   </li>
-  <li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+  <li class="nav-item dropdown {{Request::is('hoadon/*') ||  Request::is('hoadongoimon/*') ? 'show' : null}}">
+    <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
       <i class="fas fa-fw fa-folder"></i>
       <span>Hóa Đơn</span>
     </a>
-    <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+    <div class="dropdown-menu {{Request::is('hoadon/*') ||  Request::is('hoadongoimon/*') ? 'show' : null}}" aria-labelledby="pagesDropdown">
       <h6 class="dropdown-header">Hóa đơn nguyên liệu:</h6>
-      <a class="dropdown-item" href="/hoadon/danhsachhoadonnguyenlieu">Danh sách</a>
-	  <div class="dropdown-divider"></div>
-	  <h6 class="dropdown-header">Hóa đơn gọi món:</h6>
-      <a class="dropdown-item" href="/hoadongoimon/danhsachhoadongoimon">Danh sách</a>
-    
+      <a class="dropdown-item {{Request::is('hoadon/danhsachhoadonnguyenlieu') ? 'active' : null}}" href="/hoadon/danhsachhoadonnguyenlieu">Danh sách</a>
+      <div class="dropdown-divider"></div>
+      <h6 class="dropdown-header">Hóa đơn gọi món:</h6>
+      <a class="dropdown-item {{Request::is('hoadongoimon/danhsachhoadongoimon') ? 'active' : null}}" href="/hoadongoimon/danhsachhoadongoimon">Danh sách</a>
     </div>
   </li>
-  <li class="nav-item dropdown">
+  <li class="nav-item dropdown {{Request::is('thongke/*') ? 'show' : null}}">
     <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
       <i class="fas fa-fw fa-folder"></i>
       <span>Thống Kê</span>
     </a>
-    <div class="dropdown-menu" aria-labelledby="pagesDropdown">
-      <a class="dropdown-item" href="/thongke/thongketheoca">Thống kê ca</a>
-        <div class="dropdown-divider"></div>
-      <a class="dropdown-item" href="/thongke/thongketheongay">Thống kê ngày</a>
-        <div class="dropdown-divider"></div>
-      <a class="dropdown-item" href="/thongke/thongketheothang">Thống kê tháng</a>
+    <div class="dropdown-menu {{Request::is('thongke/*') ? 'show' : null}}" aria-labelledby="pagesDropdown">
+      <a class="dropdown-item {{Request::is('thongke/thongketheoca') ? 'active' : null}}" href="/thongke/thongketheoca">Thống kê ca</a>
+      <div class="dropdown-divider"></div>
+      <a class="dropdown-item {{Request::is('thongke/thongketheongay') ? 'active' : null}}" href="/thongke/thongketheongay">Thống kê ngày</a>
+      <div class="dropdown-divider"></div>
+      <a class="dropdown-item {{Request::is('thongke/thongketheothang') ? 'active' : null}}" href="/thongke/thongketheothang">Thống kê tháng</a>
     </div>
   </li>
 </ul>
