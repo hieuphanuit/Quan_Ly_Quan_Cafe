83637a78a8d0044ca5aab0fce78a49500ebd6b23
hungha1998 <16520470@gm.uit.edu.vn>
2019-05-31 00:24:59 +0700

    update layout hóa đơn gọi món

--- blog/Modules/HoaDonGoiMon/Resources/views/create.blade.php
+++ blog/Modules/HoaDonGoiMon/Resources/views/create.blade.php
@@ -25,9 +25,9 @@
 			<tr>
 
 				<td>
-					<button class="btn btn-info thucDon_btn" style="width:80%" data-id="{{$ThucDon->TenMon}}">{{$ThucDon->TenMon}}</button>
+					<button class="btn btn-info thucDon_btn" style="width:80%" data-id="{{$ThucDon->id}}" data-mon="{{$ThucDon->TenMon}}">{{$ThucDon->TenMon}}</button>
 				</td>
-				<td data-id="$ThucDon->id" >{{$ThucDon->DonGia}}</td>
+				<td data-id="$ThucDon->id">{{$ThucDon->DonGia}}</td>
 				<td></td>
 			</tr>
 			@endforeach
@@ -37,7 +37,7 @@
 	<div style="height: 30vh">
 	</div>
 	<div class="themHoaDonGoiMon">
-		<table class="table chiTietHoaDon">
+		<table class="table chiTietHoaDon " id="chiTietHoaDon">
 			<thead>
 				<tr>
 
@@ -48,52 +48,47 @@
 			</thead>
 			<tbody>
 				<tr>
-
-					<td>Món 1</td>
-					<td>
-						<div class="row">
-							<div>
-								<button class="btn btn-warning subtract-btn">-</button>
-							</div>
-							<div class="col-md-4">
-								<input class="form-control" type="number" name="soLuong" />
-							</div>
-							<div>
-								<button class="btn btn-success plus-btn">+</button>
-							</div>
-						</div>
-					</td>
-					<td>
-						<div>
-							<button class="btn btn-danger plus-btn">x</button>
-						</div>
-					</td>
 				</tr>
 			</tbody>
 		</table>
+		<div>
+		<form style="display: inline-block">
+			<button class="btn btn-success" style="width:80px; position: absolute; right: 0; margin-right:20px;" >
+				Lưu
+			</button>
+			{!! csrf_field() !!}
+		</form>
 	</div>
+	</div>
+	
 </div>
 <script>
-	$(".thucDon_btn").click(function() {
-		var tenMon = $(this).data('id');
-		$(".chiTietHoaDon").each(function() {
-			var tds = '<tr>';
-			tds += '<td>' +tenMon+ '</td>';
-			tds += '<td>';
-			tds += '<div class="row">';
-			tds += '<div>'+'<button class="btn btn-warning subtract-btn">-</button>'+'</div>';
-			tds += '<div class="col-md-4">'+'<input class="form-control" type="number" name="soLuong" />'+'</div>';
-			tds += '<div>'+'<button class="btn btn-success plus-btn">+</button>'+'</div>';
-			tds += '</div>';
-			tds += '</td>';
-			tds += '<td><div>'+'<button class="btn btn-danger plus-btn">x</button>'+'</div>';
-			tds += '</tr>';
-			if ($('tbody', this).length > 0) {
-				$('tbody', this).append(tds);
+	$(document).ready(function() {
+		$(".thucDon_btn").on("click", function() {
+			var id = $(this).data('id');
+			var mon = $(this).data('mon');
+			if ($('.child-selection[data-id="' + $(this).data('id') + '"]').length) {
+				var data = 1;
+				data += parseInt($('.child-selection[data-id="' + $(this).data('id') + '"]').val());
+				$('.child-selection[data-id="' + $(this).data('id') + '"]').val(data);
 			} else {
-				$(this).append(tds);
+				var inputfield = "<tr><td>" + mon + "</td><td><div class='row'><div><button class='btn btn-warning subtract-btn' data-id='" + id + "'>-</button></div><div class='col-md-4'><input class='child-selection form-control counter' id='counter" + id + "'style='width:60px;' type='number' data-id='" + id + "' value='1'/></div><div><button class='btn btn-success plus-btn' data-id='" + id + "'>+</button></div></div></td><td><div><button class='btn btn-danger delete-btn'>x</button></div></td></tr>";
+				$('#chiTietHoaDon').append(inputfield);
 			}
-		});	
+		});
+		$("#chiTietHoaDon").on("click", ".plus-btn", function() {
+			var id = $(this).data('id');
+			var $counter = $('#counter' + id);
+			$counter.val(parseInt($counter.val()) + 1);
+		}).on("click", ".subtract-btn", function() {
+			var id = $(this).data('id');
+			var $counter = $('#counter' + id);
+			if ($counter.val() > 1) {
+				$counter.val(parseInt($counter.val()) - 1);
+			}
+		}).on("click", ".delete-btn", function() {
+			$(this).closest('tr').remove();
+		});
 	});
 </script>
 @endsection
